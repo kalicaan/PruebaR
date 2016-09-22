f9172a2080c43ea8a3cf51bbfeaebca510f2431c
kalicaan <kalica286@gmail.com>
2016-09-22 17:15:28 -0500

    Agregando Código
    Se cambiaron los menus y algunos colores.

---
+++ js/Script.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">//<![CDATA[
+$(function(){
+    $('#slider div:gt(0)').hide();
+    setInterval(function(){
+      $('#slider div:first-child').fadeOut(0)
+         .next('div').fadeIn(1000)
+         .end().appendTo('#slider');}, 4000);
+});
+//]]></script>
